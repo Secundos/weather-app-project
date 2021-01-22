fc3efca95ffd0ae007e8ea81f413a0d76029031b
Secundos <77840586+Secundos@users.noreply.github.com>
2021-01-22 15:36:02 +0100

    add src folder

--- src/script.js
+++ src/script.js
@@ -1,4 +1,3 @@
-//obecny czas
 let now = new Date();
 let currentDate = document.querySelector("#date");
 let days = [
@@ -25,7 +24,7 @@ if (hours < 12) {
   currentDate.innerHTML = `${day} ${hours}:${minutes}PM`;
 }
 
-//parametry brane z API w czasie rzeczywistym
+
 function displayWeatherSpecyfic(response) {
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#current-temperature").innerHTML = Math.round(
@@ -44,13 +43,13 @@ function displayWeatherSpecyfic(response) {
   )} km/h`;
 }
 
-//dopiero tą funkcję łączę z api żeby móc wybrać deafultowe miasto na którym otworzy się strona
+
 function searchCity(city) {
   let apiKey = "11b09ae39e1971203074e458432047c9";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherSpecyfic);
 }
-//wyświetlanie wpisanego miasta oraz wołanie do funkcji search połączonej a API
+
 function submitedCity(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#city-input");
@@ -61,21 +60,21 @@ function submitedCity(event) {
   searchCity(city);
 }
 
-//conwerter na F
+
 function convertToFahrenheit(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#current-temperature");
   let temperature = temperatureElement.innerHTML;
   temperatureElement.innerHTML = Math.round(temperature * 1.8 + 32);
 }
-//conwerter na C
+
 function convertToCelcius(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#current-temperature");
   let temperature = temperatureElement.innerHTML;
   temperatureElement.innerHTML = Math.round((temperature - 32) / 1.8);
 }
-//funkcja połączona z API po to by po kliknięciu guzika wyświetlała się lokalizacja bieżąca
+
 function searchPosition(position) {
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
@@ -84,27 +83,24 @@ function searchPosition(position) {
 
   axios.get(apiUrl).then(displayWeatherSpecyfic);
 }
-// w funkcji zamykamy dostęp do bieżącej lokalizacji żeby nie była wołana od razu po załadowaniu
-//strony a po klknieciu guzika current
+
 function getCurrentLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(searchPosition);
 }
 
-//event dla przeliczania jednostek
+
 let celcius = document.querySelector("#celcius-link");
 celcius.addEventListener("click", convertToCelcius);
 let fahrenheit = document.querySelector("#fahrenheit-link");
 fahrenheit.addEventListener("click", convertToFahrenheit);
 
-//wyjęcie wpisywania miasta z logiki
+
 let searchForm = document.querySelector("#search-engine");
 searchForm.addEventListener("submit", submitedCity);
 
-//wyjęcie z logiki guzika do bieącej lokalizacji
-let currentLocation = document.querySelector("#current");
-currentLocation.addEventListener("click", getCurrentLocation); //po kliknięciu chcę żeby było widoczne miasto zgodne z lat lon
 
-//wołam do funkcji search żeby Warszawa była defaultowym miastem. JEst na zewnątrz
-//jakiejkolwiek funkcji więc jest wołana za każdym przeładowaniem strony
+let currentLocation = document.querySelector("#current");
+currentLocation.addEventListener("click", getCurrentLocation); 
+
 searchCity("Warsaw");
