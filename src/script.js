1e7070591bfb8f1e4deca64f01441234b81c9fe2
Secundos <77840586+Secundos@users.noreply.github.com>
2021-01-31 21:18:13 +0100

    CSS improvement

--- src/script.js
+++ src/script.js
@@ -68,7 +68,7 @@ function displayForecast(response){
  forecastElement.innerHTML += `
                 <div class="col- 2">
                 <h3>${formatHours(forecast.dt*1000)}</h3>
-                <img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}.png"  />
+                <img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}.png" id="forecast-icon" />
                 <div class= "weather-forecast-temperature">
                     <strong>${Math.round(forecast.main.temp_max)}°</strong>| ${Math.round(forecast.main.temp_min)}°
                 </div>
